d699329c4288211ab64ca474443fccb639ff58bc
ilyabelov20014@gmail.com <ilyabelov20014@gmail.com>
2021-02-26 14:24:41 +0400

    sss

---
+++ SoftwareInstalling/SoftwareInstallingDatabaseImplement/Models/Package.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftwareInstallingDatabaseImplement.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("PackageId")]
+        public virtual List<PackageComponent> PackageComponent { get; set; }
+
+        [ForeignKey("PackageId")]
+        public virtual List<Order> Order { get; set; }
+
+        [Required]
+        public string PackageName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
---
+++ SoftwareInstalling/SoftwareInstallingDatabaseImplement/SoftwareInstallingDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftwareInstallingDatabaseImplement.Models;
+
+namespace SoftwareInstallingDatabaseImplement
+{
+    public class SoftwareInstallingDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=HOME\SQLEXPRESS;Initial
+Catalog=SoftwareInstallingDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Package> Packages { set; get; }
+        public virtual DbSet<PackageComponent> PackageComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
