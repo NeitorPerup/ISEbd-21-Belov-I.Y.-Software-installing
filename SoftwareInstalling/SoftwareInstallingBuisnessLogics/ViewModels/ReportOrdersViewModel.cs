63df2beaecf87380709022ab101e607572abcc65
NeitorPerup <ilyabelov20014@gmail.com>
2021-03-12 14:32:58 +0400

    aga

---
+++ SoftwareInstalling/SoftwareInstallingBuisnessLogics/ViewModels/ReportOrdersViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SoftwareInstallingBuisnessLogic.Enums;
+
+namespace SoftwareInstallingBuisnessLogic.ViewModels
+{
+    public class ReportOrdersViewModel
+    {
+        public DateTime DateCreate { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public OrderStatus Status { get; set; }
+    }
+}
