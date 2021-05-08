e1a796f2838637fc9b4734bfacb78fa18b2889b6
NeitorPerup <ilyabelov20014@gmail.com>
2021-05-08 16:13:00 +0400

    fix

--- SoftwareInstalling/SoftwareInstallingFileImplement/Implements/OrderStorage.cs
+++ SoftwareInstalling/SoftwareInstallingFileImplement/Implements/OrderStorage.cs
@@ -38,7 +38,8 @@ namespace SoftwareInstallingFileImplement.Implements
                     (model.ClientId.HasValue && rec.ClientId == model.ClientId) ||
                     (model.FreeOrders.HasValue && model.FreeOrders.Value && rec.Status == OrderStatus.Принят) ||
                     (model.ImplementerId.HasValue && rec.ImplementerId ==
-                    model.ImplementerId && rec.Status == OrderStatus.Выполняется))
+                    model.ImplementerId && (rec.Status == OrderStatus.Выполняется ||
+                    (model.NeedComponentOrders.HasValue && model.NeedComponentOrders.Value && rec.Status == OrderStatus.Требуются_материалы))))
                     .Select(CreateModel).ToList();
         }
 
